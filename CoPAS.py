b54148789876ddb8978cf4ac6a4570a38c9710a6
delene@aero.und.edu <delene@aero.und.edu>
2019-09-20 11:29:59 -0500

    Using python3.6 instead of python3.

--- CoPAS.py
+++ CoPAS.py
@@ -97,6 +97,7 @@ MODIFICATIONS:
   David Delene <delene@aero.und.edu> - 2019/09/20
     Updated to using wget to get ADPAA.tar.gz.
     Added additional python module check.
+    Changed to using python3.6 instead of just python3.
 
 REFERENCES:
   Airborne Data Processing and Analysis (ADPAA)
